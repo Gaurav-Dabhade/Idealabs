0f0bf077a787924de0f88ee837745881e6bc6ee6
dabhadegaurav <66509220+dabhadegaurav@users.noreply.github.com>
2020-11-08 14:01:13 +0530

    deleted public videos and images

--- resources/views/Voice/homevoice.blade.php
+++ resources/views/Voice/homevoice.blade.php
@@ -691,8 +691,11 @@ Founder of idealabs (At bottom of image)
          headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
          success:function(data){
             $('#name').html(data.title);
-             $('#content').html('<video width="500" height="400" controls autoplay><source src="/Videos/""'+data.video+'" type="video/mp4"></video>');
+            var videoFile = '/Videos/'+data.video;
 
+            $('#content video source').attr('src', videoFile);
+
+            $('#content video').get(0).load();
 
          }
       });
--- resources/views/admin/include/footer.blade.php
+++ resources/views/admin/include/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12">
-                © 2018 - <script>document.write(new Date().getFullYear())</script> Lexa <span class="d-none d-sm-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand.</span>
+                © 2018 - <script>document.write(new Date().getFullYear())</script> Lexa <span class="d-none d-sm-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by Idealabs.</span>
             </div>
         </div>
     </div>
